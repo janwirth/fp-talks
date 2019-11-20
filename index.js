e8def0323f78e2d6867aaa3dfc2bedb96c96823b
Jan Wirth <jan@scalab.app>
2019-11-20 17:47:22 +0000

    Add resources

--- index.js
+++ index.js
@@ -1,6 +1,17 @@
 /*
  * FP Course for JS developers
+ * - Intro: Working hours: Imperative, functional, declarative
+ * - not wanna go into esoterics, no philosophy, break it down simple but still benefit from abstraction
+ * - functional STYLE, tried to keep it idiomatic
+ * - why is it useful
+ *   - we are already using it - declarative / reactive / functional views
+ *   - less effort because you have to keep less memory in the dev's mind
+ * - is it available?
+ *   - it's the big stuff
+ * - not saying either or the other is inferior - they complement each other
  * - pure functions & immutable data going hand in hand
+ *      - pure function is just a function - only a function. nothing else. The mathematical definition
+ *      - only direct inputs vary the value. There may be constants
  * - first-class and higher-order functions
  * - apply some curry
  * - composition with and without points
@@ -9,6 +20,21 @@
  * - state of Typescript
  *   - composition operators
  *   - pattern matching for recursion and union types
+ * key learnings
+ *  - pure functions
+ *  - higher-order functions
+ *  - composition and point-free style
+ *  - immutable data
+ *  - functors & monads
+ *  - recursion and destructuring
+ */
+
+ /* notes from Anjana Talk
+ *   - what is a paradigm: a worldview
+ *   - state and time
+ *   - OOP - keep state, communicate through messages
+ *      - each house can call a line
+ *   - FP = one simple idea = PURE functions
  */
 
 
@@ -16,11 +42,15 @@
 const _ = require('ramda')
 const promiseTry = require("es6-promise-try");
 async function main() {
+    // arrays
+    // iterator code and worker code
+    // TODO: list iteration example here
+    // TODO: same example
 
     // CONTAINERS
 
     // mappable
-    await [1,2,3].map(num => num * 2)
+    [1,2,3].map(num => num * 2)
 
     // thenable
     Promise.resolve(1).then(num => num * 2)
@@ -32,7 +62,7 @@ async function main() {
     Promise.resolve(1).then(num => Promise.resolve(num * 2));
 
     // explicit flattening
-    [1,2,3].flatMap(x => [x * 2, x + 1])
+    flatMap(x => [x * 2, x + 1], [1,2,3])
 
     // idiomatic async
     await Promise.resolve(1)
@@ -51,6 +81,8 @@ async function main() {
 function map (fn) {
     return mappable => mappable.map(fn)
 }
+const flatten = arr => arr.reduce((a, b) => [...a, ...b])
+const flatMap = fn => arr => arr.map(fn)
 
 
 function inspect (arg) {
