0db9075fe673cd79ac109adddc5c7e127164b998
Jan Wirth <jan.wirth@mercedes-benz.io>
2019-11-22 11:45:47 +0000

    Fix app

--- app/main.js
+++ app/main.js
@@ -4,6 +4,7 @@ import $ from "sanctuary-def"
 
 // we count from 0
 
+const StateType = $.FiniteNumber
 // initState :: FiniteNumber
 const initState = 0
 
@@ -11,19 +12,19 @@ const initState = 0
 // log :: a -> a
 const log = d => {console.log(d); return d}
 
-
-// parseState :: Nullable String -> Maybe FiniteNumber
+// parseState :: Nullable String -> Maybe StateType
 const parseState = str =>
     {
         try {return S.Just(JSON.parse(str))}
         catch (e) {return S.Nothing}
     }
 
-// loadState :: Nullable String -> FiniteNumber
+// loadState :: Nullable String -> StateType
 const loadState =
     S.pipe([
         log
       , parseState
+      , S.mapMaybe(parsed => S.is (StateType) ? S.Just (parsed) : S.Nothing)
       , S.fromMaybe(initState)
     ])
 
@@ -31,7 +32,7 @@ const init =
     loadState(localStorage.state)
 
 
-// persist :: FiniteNumber -> FiniteNumber
+// persist :: StateType -> StateType
 const persist = state => {
     localStorage.state = JSON.stringify(state)
     return state
@@ -39,11 +40,11 @@ const persist = state => {
 
 const eventToPair = (state, event) => S.Pair(state)(event)
 
-// update :: FiniteNumber -> FiniteNumber
+// update :: StateType -> StateType
 const update = transformation =>
     S.unchecked.pipe([
         eventToPair
-      , S.mapFst(S.pipe([transformation, persist]))
+      , S.fst
       , transformation
       , persist
     ])
